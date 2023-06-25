5833961f8cdf251e74bfb9dc77ade8655446e840
SagiKimhi <sagi.kim5@gmail.com>
2023-06-25 07:49:25 -0400

    Fixed the dot function - dst was not being initialized to value 0 which was causing consecutive calls to the function to give incorrect results

--- src/nn_matrix.c
+++ src/nn_matrix.c
@@ -10,9 +10,10 @@ nn_matrix_t nn_matrix_alloc(size_t num_rows, size_t num_cols)
 {
     nn_matrix_t m;
 
-    m.rows  = num_rows;
-    m.cols  = num_cols;
-    m.data  = NN_MALLOC(sizeof(*m.data) * num_rows * num_cols);
+    m.rows      = num_rows;
+    m.cols      = num_cols;
+    m.stride    = num_cols;
+    m.data      = NN_MALLOC(sizeof(*m.data) * num_rows * num_cols);
 
     NN_ASSERT(m.data);
 
@@ -67,12 +68,17 @@ void nn_matrix_dot(nn_matrix_t dst, nn_matrix_t a, nn_matrix_t b)
 
     size_t n = a.cols;
 
-    for (size_t i = 0; i < dst.rows; i++)
-        for (size_t j = 0; j < dst.cols; j++)
-            for (size_t k = 0; k < n; k++)
-                NN_MATRIX_AT(dst, i, j) = (
+    nn_matrix_memset(dst, 0);
+
+    for (size_t i = 0; i < dst.rows; i++) {
+        for (size_t j = 0; j < dst.cols; j++) {
+            for (size_t k = 0; k < n; k++) {
+                NN_MATRIX_AT(dst, i, j) += (
                         NN_MATRIX_AT(a, i, k) * NN_MATRIX_AT(b, k, j)
                 );
+            }
+        }
+    }
 }
 
 void nn_matrix_sum(nn_matrix_t dst, nn_matrix_t m)
@@ -81,18 +87,22 @@ void nn_matrix_sum(nn_matrix_t dst, nn_matrix_t m)
     NN_ASSERT(m.cols == dst.cols);
     NN_ASSERT(m.data && dst.data);
     
-    for (size_t row = 0; row < dst.rows; row++)
-        for (size_t col = 0; col < dst.cols; col++)
+    for (size_t row = 0; row < dst.rows; row++) {
+        for (size_t col = 0; col < dst.cols; col++) {
             NN_MATRIX_AT(dst, row, col) += NN_MATRIX_AT(m, row, col);
+        }
+    }
 }
 
 extern void nn_matrix_sigmoid(nn_matrix_t m)
 {
     NN_ASSERT(m.data);
 
-    for (size_t row = 0; row < m.rows; row++)
-        for (size_t col = 0; col < m.cols; col++)
+    for (size_t row = 0; row < m.rows; row++) {
+        for (size_t col = 0; col < m.cols; col++) {
             NN_MATRIX_AT(m, row, col) = sigmoidf(NN_MATRIX_AT(m, row, col));
+        }
+    }
 }
 
 /* ----------------
@@ -107,6 +117,16 @@ extern void nn_matrix_copy(nn_matrix_t dst, nn_matrix_t src)
     for (size_t row = 0; row < dst.rows; row++)
         for (size_t col = 0; col < dst.cols; col++)
             NN_MATRIX_AT(dst, row, col) = NN_MATRIX_AT(src, row, col);
+}
+
+nn_matrix_t nn_matrix_row(nn_matrix_t m, size_t row)
+{
+    return (nn_matrix_t) {
+        .rows   = 1,
+        .cols   = m.cols,
+        .stride = m.stride,
+        .data   = &NN_MATRIX_AT(m, row, 0),
+    };
 }
 
 void nn_matrix_print(nn_matrix_t m, char *name)
