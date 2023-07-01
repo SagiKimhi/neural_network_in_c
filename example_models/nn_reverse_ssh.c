15c0edbc0dca0c0c14fd369d99d8c1679fe3d5aa
SagiKimhi <sagi.kim5@gmail.com>
2023-07-01 15:45:42 -0400

    Added hdr and source code for basic io handling operations and cleared source code warnings of nn_reverse_ssh.c

--- example_models/nn_reverse_ssh.c
+++ example_models/nn_reverse_ssh.c
@@ -1,6 +1,6 @@
 //#define NN_BACKPROP_TRADITIONAL
 #include <nn.h>
-typedef void nn_load_data_func_t(char *data_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_out, size_t range);
+typedef void nn_load_data_func_t(char *data_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_out);
 #define NN_LOAD_DATA_FUNC_T nn_load_data_func_t
 #define NN_LOAD_DATA_FUNC_ARGS "", &ts_in, &ts_out, 1
 
@@ -9,10 +9,8 @@ typedef void nn_load_data_func_t(char *data_filepath, nn_matrix_t *ts_in, nn_mat
  * Includes:
  * --------- */
 #include <nn_render.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <nn_io.h>
 
 
 /* --------
@@ -37,7 +35,6 @@ typedef enum {
 /* ----------
  * Constants:
  * ---------- */
-const size_t    default_optional_range  = 1;
 const size_t    nn_input_size           = 96;
 const size_t    nn_output_size          = 384;
 const uint8_t   args_are_valid          = 0;
@@ -55,21 +52,18 @@ const size_t    max_priv_key_path_len   = (
 /* --------------------
  * Function Prototypes:
  * -------------------- */
-uint8_t validate_cmdline_args(int argc, const char **argv, size_t *optional_range);
-uint8_t validate_filepath_arg(const char *keypair_path_arg, size_t optional_range);
-uint8_t validate_optional_cmdline_arg(const char *keypair_path_arg, const char *optional_arg, size_t *range);
+uint8_t validate_cmdline_args(int argc, const char **argv);
 static void report_cmdline_arg_err(cmdline_args_err_flags flag, char *flag_name);
-void load_training_data(char *key_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_out, size_t optional_range);
+void load_training_data(char *key_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_out);
 void print_results(nn_t nn, nn_matrix_t ts_in, nn_matrix_t ts_out);
 
 
 int main(int argc, char **argv)
 {
     srand(time(0));
-    size_t      optional_arg_range  = default_optional_range;
     nn_matrix_t ts_in, ts_out;
     
-    load_training_data("encrypted_text.enc", &ts_in, &ts_out, optional_arg_range);
+    load_training_data("encrypted_text.enc", &ts_in, &ts_out);
 
     if (ts_in.data == NULL || ts_out.data == NULL)
         return EXIT_FAILURE;
@@ -124,121 +118,76 @@ void print_results(nn_t nn, nn_matrix_t ts_in, nn_matrix_t ts_out)
 }
 
 
-void load_training_data(char *key_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_out, size_t optional_range)
+void load_training_data(char *key_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_out)
 {
 
-    uint8_t err_flag                    = 0;
-    size_t  private_key_offset          = 0;
-    size_t  keypair_path_len            = 0;
-    size_t  public_key_len              = 0;
-    size_t  private_key_len             = 0;
-    char    public_key_path[BUFSIZ+1]   = {0};
-    char    private_key_path[BUFSIZ+1]  = {0};
-    uint8_t public_key_buf[BUFSIZ + 1]  = {0};
-    char    private_key_buf[BUFSIZ + 1] = {0};
-    FILE    *input_data_file            = NULL;
-    FILE    *output_data_file           = NULL;
+    uint8_t err_flag                        = 0;
+    size_t  decrypted_text_offset           = 0;
+    size_t  encrypted_text_len              = 0;
+    size_t  decrypted_text_len              = 0;
+    uint8_t encrypted_text_buf[BUFSIZ + 1]  = {0};
+    char    decrypted_text_buf[BUFSIZ + 1]  = {0};
+    FILE    *input_data_file                = NULL;
+    FILE    *output_data_file               = NULL;
 
-    keypair_path_len = strnlen(key_filepath, max_priv_key_path_len + 1);
-
-    snprintf(public_key_path, max_priv_key_path_len, "/home/kali/project/nn_training_data/ssh_data/data/text.enc");
-    snprintf(private_key_path, max_priv_key_path_len, "/home/kali/project/nn_training_data/ssh_data/data/text.txt");
-
-    for (size_t i = 1; i <= optional_range; i++) {
-        if (optional_range != default_optional_range) {
-            sprintf(&private_key_path[keypair_path_len-1], "%zu", i);
-            sprintf(&public_key_path[keypair_path_len-1], "%zu%s", i, public_key_postfix);
-        }
-
-        if (!(input_data_file = fopen(public_key_path, "r"))) {
-            fprintf(
-                stderr, 
-                "Failed to open public key file.\nPath was: %s\n",
-                public_key_path
-            );
-            err_flag = 1;
-            break;
-        }
-
-        if (!(output_data_file = fopen(private_key_path, "r"))) {
-            fprintf(
-                stderr, 
-                "Failed to open private key file.\nPath was: %s\n",
-                private_key_path
-            );
-            err_flag = 1;
-            break;
-        }
-
-        if (!fgets(public_key_buf, BUFSIZ, input_data_file)) {
-            fprintf(stderr, "Failed to read public key from file due to newline in begining of file or an empty file\n");
-            err_flag = 1;
-            break;
-        }
-
-        for (size_t private_key_iter = 0; fgets(&private_key_buf[private_key_offset], BUFSIZ + 1 - private_key_offset, output_data_file); private_key_iter++) {
-            size_t prev_offset = 0;
-
-            if (!private_key_offset) {
-                private_key_offset = strlen(private_key_buf);
-                continue;
-            }
-
-            prev_offset = private_key_offset;
-            private_key_offset += strlen(&private_key_buf[prev_offset]);
-        }
-
-        if (!public_key_len) {
-            public_key_len = strlen(public_key_buf);
-        }
-        else if (public_key_len != strlen(public_key_buf)) {
-            fprintf(stderr, "Mismatch between public key sizes\nprevious length was: %zu, "\
-                    "but length of key from file %s is %lu.\nPlease validate your keys and try again.\n",
-                    public_key_len, public_key_path, strlen(public_key_buf));
-            err_flag = 1;
-            break;
-        }
-        
-        if (!private_key_len) {
-            private_key_len = strlen(private_key_buf);
-
-            if (private_key_len != private_key_offset) {
-                err_flag = 1;
-                fprintf(stderr, "private_key_len: %zu, private_key_offset: %zu\n"\
-                        "private_key: %s\n",
-                        private_key_len, private_key_offset, private_key_buf);
-                break;
-            }
-        }
-        else if (private_key_len != private_key_offset) {
-            fprintf(stderr, "Mismatch between private key sizes\nprevious length was: %zu, "\
-                    "but length of key from file %s is %lu.\nPlease validate your keys and try again.\n",
-                    private_key_len, private_key_path, strlen(private_key_buf));
-            err_flag = 1;
-            break;
-        }
-
-        private_key_offset = 0;
-
-        if (ts_in->data == NULL) {
-            *ts_in = nn_matrix_alloc(optional_range, nn_input_size);
-        }
-
-        if (ts_out->data == NULL) {
-            *ts_out = nn_matrix_alloc(optional_range, nn_output_size);
-        }
-
-        for (size_t j = 0; j < ts_in->cols; j++)
-            NN_MATRIX_AT(*ts_in, i-1, j) = (j < public_key_len ? ( (public_key_buf[j]) / (float)(unsigned)UINT8_MAX): 0);
-
-        for (size_t j = 0; j < ts_out->cols; j++)
-            NN_MATRIX_AT(*ts_out, i-1, j) = ( j < private_key_len ? ((private_key_buf[j]) / (float)CHAR_MAX): 0);
-
-        fclose(input_data_file);
-        fclose(output_data_file);
-        input_data_file = NULL;
-        output_data_file = NULL;
+    if (!(input_data_file = nn_io_fopen(key_filepath, ".enc", "r"))) {
+        err_flag = 1;
     }
+
+    if (!(output_data_file = nn_io_fopen(key_filepath, ".txt", "r"))) {
+        err_flag = 1;
+    }
+
+    if (!err_flag && !fgets((char*)encrypted_text_buf, BUFSIZ, input_data_file)) {
+        fprintf(stderr, "Failed to read encrypted text from file due to newline in begining of file or an empty file\n");
+        err_flag = 1;
+    }
+
+    while (!err_flag && fgets(&decrypted_text_buf[decrypted_text_offset], BUFSIZ + 1 - decrypted_text_offset, output_data_file)) {
+        size_t prev_offset = 0;
+
+        if (!decrypted_text_offset) {
+            decrypted_text_offset = strlen(decrypted_text_buf);
+            continue;
+        }
+
+        prev_offset = decrypted_text_offset;
+        decrypted_text_offset += strlen(&decrypted_text_buf[prev_offset]);
+    }
+
+    if (!err_flag && !encrypted_text_len) {
+        encrypted_text_len = strlen((char*)encrypted_text_buf);
+    }
+    
+    if (!err_flag && !decrypted_text_len) {
+        decrypted_text_len = strlen(decrypted_text_buf);
+
+        if (decrypted_text_len != decrypted_text_offset) {
+            err_flag = 1;
+            fprintf(stderr, "private_key_len: %zu, private_key_offset: %zu\n"\
+                    "private_key: %s\n",
+                    decrypted_text_len, decrypted_text_offset, decrypted_text_buf);
+        }
+    }
+
+    if (ts_in->data == NULL) {
+        *ts_in = nn_matrix_alloc(1, nn_input_size);
+    }
+
+    if (ts_out->data == NULL) {
+        *ts_out = nn_matrix_alloc(1, nn_output_size);
+    }
+
+    for (size_t j = 0; j < ts_in->cols; j++)
+        NN_MATRIX_AT(*ts_in, 0, j) = (j < encrypted_text_len ? ( (encrypted_text_buf[j]) / (float)(unsigned)UINT8_MAX): 0);
+
+    for (size_t j = 0; j < ts_out->cols; j++)
+        NN_MATRIX_AT(*ts_out, 0, j) = ( j < decrypted_text_len ? ((decrypted_text_buf[j]) / (float)CHAR_MAX): 0);
+
+    fclose(input_data_file);
+    fclose(output_data_file);
+    input_data_file = NULL;
+    output_data_file = NULL;
 
     if (err_flag) {
         if (ts_in->data){
@@ -253,89 +202,20 @@ void load_training_data(char *key_filepath, nn_matrix_t *ts_in, nn_matrix_t *ts_
             fclose(input_data_file);
         if (output_data_file)
             fclose(output_data_file);
-        
-        return;
     }
 }
 
-uint8_t validate_cmdline_args(int argc, const char **argv, size_t *optional_range)
+uint8_t validate_cmdline_args(int argc, const char **argv)
 {
     if (argc < min_nof_cmdline_args) {
         REPORT_CMDLINE_ARG_ERR(NOT_ENOUGH_ARGS_F);
         fprintf( stderr, 
-            "Usage: %s [path to private key] {Optional range: 1,10}\n",
+            "Usage: %s [path to file (without the file extension)]\n",
             argv[0] 
         );
         return (!args_are_valid);
     }
-
-    if (argc > min_nof_cmdline_args) {
-        return (
-            validate_optional_cmdline_arg( argv[keypair_path_arg_idx], 
-                argv[optional_range_arg_idx], optional_range )
-        );
-    }
-
-    return validate_filepath_arg(argv[keypair_path_arg_idx], *optional_range);
-}
-
-uint8_t validate_optional_cmdline_arg(const char *keypair_path_arg, const char *optional_arg, size_t *range)
-{
-    NN_ASSERT(keypair_path_arg && optional_arg);
-
-    size_t arg_len = strnlen(optional_arg, max_optional_arg_len + 1);
-
-    if (arg_len > max_optional_arg_len) {
-        REPORT_CMDLINE_ARG_ERR(INVALID_OPTIONAL_ARG_LENGTH_F);
-        return (!args_are_valid);
-    }
     
-    *range = atoi(optional_arg);
-
-    if (!(*range)) {
-        *range = default_optional_range;
-        REPORT_CMDLINE_ARG_ERR(INVALID_OPTIONAL_ARG_SYNTAX_F);
-        return (!args_are_valid);
-    }
-
-    return validate_filepath_arg(keypair_path_arg, *range);
-}
-
-uint8_t validate_filepath_arg(const char *keypair_path_arg, size_t optional_range)
-{
-    size_t  keypair_path_len;
-    char    public_key_buf[BUFSIZ+1]    = {0};
-    char    private_key_buf[BUFSIZ+1]   = {0};
-
-    keypair_path_len = strnlen(keypair_path_arg, max_priv_key_path_len + 1);
-
-    if (!keypair_path_len || keypair_path_len > max_priv_key_path_len) {
-        REPORT_CMDLINE_ARG_ERR(INVALID_KEYPAIR_PATH_LEN_F);
-        return (!args_are_valid);
-    }
-
-    snprintf(private_key_buf, keypair_path_len + 1, "%s", keypair_path_arg);
-    snprintf(public_key_buf, keypair_path_len + 1, "%s%s", keypair_path_arg, public_key_postfix);
-
-    for (size_t i = 1; i <= optional_range; i++) {
-        if (optional_range != default_optional_range) {
-            sprintf(&private_key_buf[keypair_path_len-1], "%zu", i);
-            sprintf(&public_key_buf[keypair_path_len-1], "%zu%s", i, public_key_postfix);
-        }
-
-        if (access(public_key_buf, R_OK)) {
-            REPORT_CMDLINE_ARG_ERR(INVALID_PUBLIC_KEY_FILE_PATH_F);
-            fprintf(stderr, "path was: %s\n", public_key_buf);
-            return (!args_are_valid);
-        }
-
-        if (access(private_key_buf, R_OK)) {
-            REPORT_CMDLINE_ARG_ERR(INVALID_PRIVATE_KEY_FILE_PATH_F);
-            fprintf(stderr, "path was: %s\n", private_key_buf);
-            return (!args_are_valid);
-        }
-    }
-
     return args_are_valid;
 }
 
---
+++ hdr/nn_io.h
@@ -0,0 +1,55 @@
+#ifndef NN_IO_H_
+#define NN_IO_H_
+
+/* ---------
+ * Includes:
+ * --------- */
+#include <nn_common_includes.h>
+
+
+/* --------
+ * Defines:
+ * -------- */
+
+
+/* ---------
+ * Typedefs:
+ * --------- */
+
+
+/* ----------------------
+ * Function Declarations:
+ * ---------------------- */  
+
+/* Function: nn_io_fopen
+ *
+ * Attempts to open the specified ~filename~.~file_extension~
+ * under the specified ~mode~ argument.
+ * Upon success, the file pointer is returned.
+ * Upon failure, an error is printed and NULL is returned. */
+extern FILE *nn_io_fopen(const char *filename, const char *file_extension, const char *mode);
+
+/* Function: nn_io_get_line
+ *
+ * TLDR: 
+ * like fgets, but discards trailing whitespace, and discards the newline character
+ *
+ * Get the next line of input from ~stream~ into ~buf~ while skipping
+ * and discarding any trailing whitespace characters.
+ * ~bufsize~ specifies the max number of characters to be read into ~buf~. 
+ * The function stops when either bufsize limit is reached, newline character 
+ * is reached (and skipped), or EOF is reached. 
+ * The return value is the total number of characters read and/or skipped. */
+extern int nn_io_get_line(char *buf, size_t bufsize, FILE *stream);
+
+/* Function: nn_io_skip_whitespace
+ *
+ * skips all trailing spaces in stream up until reaching a
+ * newline or a non whitespace character.
+ * Returns the latest character encountered, or EOF upon failure or if EOF 
+ * was encountered.
+ * Non whitespace characters are pushed back into stream with ungetc. */
+extern int nn_io_skip_whitespace(FILE *stream);
+
+#endif /* NN_IO_H_ */
+
